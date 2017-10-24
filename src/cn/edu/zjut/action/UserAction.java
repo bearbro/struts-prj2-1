5e5accf8b7835e3a147914c7dad910046fa715f9
bearbro <773854587@qq.com>
2017-10-24 15:02:14 +0800

    通过接口ServletContextAware、ServletRequestAware、ServletResponseAware直接访问Servlet API

--- src/cn/edu/zjut/action/UserAction.java
+++ src/cn/edu/zjut/action/UserAction.java
@@ -4,6 +4,10 @@ import cn.edu.zjut.bean.UserBean;
 import cn.edu.zjut.service.UserService;
 import com.opensymphony.xwork2.ActionSupport;
 import org.apache.struts2.ServletActionContext;
+import org.apache.struts2.interceptor.ServletRequestAware;
+import org.apache.struts2.interceptor.ServletResponseAware;
+import org.apache.struts2.util.ServletContextAware;
+
 import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,13 +21,31 @@ import java.util.regex.Pattern;
 /**
  * Created by Bro、小熊 on 2017/10/2.
  */
-public class UserAction extends ActionSupport {
+public class UserAction extends ActionSupport
+        implements ServletContextAware, ServletRequestAware, ServletResponseAware {
     private UserBean loginUser;
     private Integer count = Integer.valueOf(0);
-    private HttpServletRequest request= ServletActionContext.getRequest();
-    private HttpServletResponse response=ServletActionContext.getResponse();
-    private HttpSession session=request.getSession();
-    private ServletContext application=ServletActionContext.getServletContext();
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+    private HttpSession session;
+    private ServletContext application;
+
+    @Override
+    public void setServletResponse(HttpServletResponse response) {
+        this.response = response;
+    }
+
+    @Override
+    public void setServletContext(ServletContext application) {
+        this.application = application;
+    }
+
+    @Override
+    public void setServletRequest(HttpServletRequest request) {
+        this.request = request;
+//        session=request.getSession();
+    }
+
 
     public UserAction() {
         System.out.print("创建了一个UserAction类对象.\n");
@@ -121,6 +143,7 @@ public class UserAction extends ActionSupport {
     }
 
     public String login() {
+        session = request.getSession();
         Integer counter = (Integer) application.getAttribute("counter");
         if (counter == null) {
             counter = 1;
@@ -140,4 +163,6 @@ public class UserAction extends ActionSupport {
             return "logfail";
         }
     }
+
+
 }
